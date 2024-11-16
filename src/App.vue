3c290d4c49173e6b6a12372dfcb7107f1f2aecb4
Khoanhps39175 <Khoanhps39175@gmail.com>
2024-11-17 00:06:26 +0700

    Add new project Lab4

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- Hello Word -->
+<!-- <script setup>
+  import Hello from './components/Hello.vue';
+</script>
+<template>
+  <Hello></Hello>
+</template>
+<style scoped></style> -->
+
+<!-- Bài 3 -->
+<!-- <script setup>
+  import Bai3 from './components/Bai3.vue';
+</script>
+<template>
+  <Bai3 />
+</template> -->
+
+<!-- Bài 4 -->
+<script setup>
+  import Bai4 from './components/Bai4.vue';
+</script>
+<template>
+  <Bai4 />
+</template>
